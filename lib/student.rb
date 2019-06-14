e272c7e078629a6c02ff4cd88f4fcd4504a036e7
AJ Guiles <ashiguiles@gmail.com>
2019-06-14 03:53:12 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -47,12 +47,12 @@ class Student
     student.save
   end
 
-  # def self.new_from_db(row)
-  #   id = row[0]
-  #   name = row[1]
-  #   grade = row[2]
-  #   self.new(id, name, grade)
-  # end
+  def self.new_from_db(row)
+    id = row[0]
+    name = row[1]
+    grade = row[2]
+    self.new(id, name, grade)
+  end
 
   # def self.find_by_name(name)
   #   # find the student in the database given a name
